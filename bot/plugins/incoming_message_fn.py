9ec08c687aa4d72c11cf85c74381a85b8348b517
abai254 <84307584+abai254@users.noreply.github.com>
2021-08-12 13:42:47 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -84,13 +84,6 @@ async def incoming_start_message_f(bot, update):
                 parse_mode="markdown"
             )
             return
-        except Exception:
-            await message.reply_text(
-                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
-                parse_mode="markdown",
-                disable_web_page_preview=True
-            )
-            return
     await bot.send_message(
         chat_id=update.chat.id,
         text=Localisation.START_TEXT,
